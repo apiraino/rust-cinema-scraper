3bad58564bd73c3482ba61b9f84487b27268f1ac
Antonio Piraino <antonio.piraino@quokky.com>
2017-05-02 00:53:55 +0200

    add src and requirements

---
+++ README.md
@@ -0,0 +1,1 @@
+A web crawler that gets the cinema movie list every week, save it into a DB and expose an RSS feed for public consumption.
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+#[macro_use]
+extern crate log;
+extern crate clap;
+extern crate scraper;
+extern crate hyper;
+
+use std::process;
+use std::io::Read;
+use std::io::Write;
+use std::fs::File;
+
+use log::{LogRecord, LogLevel, LogMetadata, SetLoggerError, LogLevelFilter};
+use clap::{App, Arg};
+use hyper::Client;
+use hyper::header::Connection;
+use scraper::{Html, Selector};
+
+struct SimpleLogger;
+
+impl log::Log for SimpleLogger {
+    fn enabled(&self, metadata: &LogMetadata) -> bool {
+        metadata.level() <= LogLevel::Trace
+    }
+
+    fn log(&self, record: &LogRecord) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+}
+
+pub fn init_log() -> Result<(), SetLoggerError> {
+    log::set_logger(|max_log_level| {
+        max_log_level.set(LogLevelFilter::Debug);
+        Box::new(SimpleLogger)
+    })
+}
+
+
+fn main() {
+
+    match init_log() {
+        Ok(_) => println!("logging started."),
+        Err(err) => println!("error starting logger: {}", err)
+    };
+
+    let matches = App::new("Visionario feed crawler")
+        .version("0.1")
+        .author("Antonio Piraino")
+        .about("Grab the weekly cinema feed and store into DB")
+        .arg(Arg::with_name("date_from")
+             .short("f")
+             .long("date_from")
+             .help("Date FROM to start crawling")
+             .takes_value(true))
+        .get_matches();
+    debug!("Got param: {}", matches.value_of("date_from").unwrap());
+
+    // Get HTML file
+    let date = matches.value_of("date_from").unwrap();
+    let url = format!("http://visionario.movie/calendario-settimanale/?data={}", date);
+    let client = Client::new();
+    let mut response = client.get(&url).
+        header(Connection::close()).send().unwrap_or_else(|e| {
+            error!("Error occurred: {}", e);
+            process::exit(1);
+        });
+    let mut body = String::new();
+    response.read_to_string(&mut body).unwrap();
+
+    // Local debugging without hammering the (unsuspecting) website
+    // let filename = format!("{}.html", date);
+    // let mut fp = File::create(filename).unwrap();
+    // match fp.write_all(body.as_bytes()) {
+    //     Ok(_) => debug!("File written"),
+    //     Err(err) => debug!("Failed file write: {}", err)
+    // };
+    // let mut f = File::open(filename).unwrap();
+    // f.read_to_string(&mut body).unwrap();
+
+    // "Now you have two problems" (cit. Jamie Zawinski)
+    let document = Html::parse_document(&body);
+    let movie_row = Selector::parse("div.singoloFilmWrapper").unwrap();
+    let title_sel = Selector::parse("div.titoloGriglia").unwrap();
+    for row in document.select(&movie_row).skip(1) {
+        let title_el = row.select(&title_sel).next().unwrap();
+        let title = title_el.text().next().unwrap().trim();
+        println!("Got movie title: {}", title);
+    }
+}
