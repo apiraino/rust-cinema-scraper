d91388b6b7b4b66b2e32c4ebdc0b6fdd50b14a14
Antonio Piraino <antonio.piraino@quokky.com>
2017-05-03 02:21:30 +0200

    add more HTML parsing

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ impl log::Log for SimpleLogger {
 
 pub fn init_log() -> Result<(), SetLoggerError> {
     log::set_logger(|max_log_level| {
-        max_log_level.set(LogLevelFilter::Debug);
+        max_log_level.set(LogLevelFilter::Info);
         Box::new(SimpleLogger)
     })
 }
@@ -56,35 +56,70 @@ fn main() {
         .get_matches();
     debug!("Got param: {}", matches.value_of("date_from").unwrap());
 
-    // Get HTML file
     let date = matches.value_of("date_from").unwrap();
-    let url = format!("http://visionario.movie/calendario-settimanale/?data={}", date);
-    let client = Client::new();
-    let mut response = client.get(&url).
-        header(Connection::close()).send().unwrap_or_else(|e| {
-            error!("Error occurred: {}", e);
-            process::exit(1);
-        });
     let mut body = String::new();
-    response.read_to_string(&mut body).unwrap();
+
+    // Get HTML file
+    // let url = format!("http://visionario.movie/calendario-settimanale/?data={}", date);
+    // let client = Client::new();
+    // let mut response = client.get(&url).
+    //     header(Connection::close()).send().unwrap_or_else(|e| {
+    //         error!("Error occurred: {}", e);
+    //         process::exit(1);
+    //     });
+    // response.read_to_string(&mut body).unwrap();
 
     // Local debugging without hammering the (unsuspecting) website
-    // let filename = format!("{}.html", date);
-    // let mut fp = File::create(filename).unwrap();
+    let filename = format!("{}.html", date);
+    // let mut fp = File::create(&filename).unwrap();
     // match fp.write_all(body.as_bytes()) {
     //     Ok(_) => debug!("File written"),
     //     Err(err) => debug!("Failed file write: {}", err)
     // };
-    // let mut f = File::open(filename).unwrap();
-    // f.read_to_string(&mut body).unwrap();
+    let mut f = File::open(filename).unwrap();
+    f.read_to_string(&mut body).unwrap();
 
     // "Now you have two problems" (cit. Jamie Zawinski)
-    let document = Html::parse_document(&body);
-    let movie_row = Selector::parse("div.singoloFilmWrapper").unwrap();
+    let mut document = Html::parse_document(&body);
+    let movie_row_sel = Selector::parse("div.singoloFilmInfo").unwrap();
     let title_sel = Selector::parse("div.titoloGriglia").unwrap();
-    for row in document.select(&movie_row).skip(1) {
+    let director_sel = Selector::parse("div.registaGriglia").unwrap();
+    let timetable_sel = Selector::parse("div.orari").unwrap();
+    let li_sel = Selector::parse("li").unwrap();
+    let href_sel = Selector::parse("a").unwrap();
+    let mut href = String::new();
+    for row in document.select(&movie_row_sel) {
+
         let title_el = row.select(&title_sel).next().unwrap();
         let title = title_el.text().next().unwrap().trim();
-        println!("Got movie title: {}", title);
+        let director_el = row.select(&director_sel).next().unwrap();
+        let director = director_el.text().next().unwrap().trim();
+
+        let timetable_el = row.select(&timetable_sel).next().unwrap();
+        let mut timetable = String::new();
+        for t in timetable_el.select(&li_sel) {
+            timetable += t.text().next().unwrap();
+            timetable += " ";
+        }
+
+        let href_el = row.select(&href_sel).next().unwrap();
+        href = format!("{}", href_el.value().attr("href").unwrap());
+
+        info!("title={} director={}, orari={} link= {}",
+              title, director, timetable, href);
     }
+
+    // TODO: GET movie detail page
+    f = File::open("movie_detail.html").unwrap();
+    f.read_to_string(&mut body).unwrap();
+    document = Html::parse_document(&body);
+    // retrieve plot
+    let movie_plot_sel = Selector::parse("div.plot").unwrap();
+    let p_sel = Selector::parse("p").unwrap();
+    let movie_plot_el = document.select(&movie_plot_sel).next().unwrap();
+    for item in movie_plot_el.select(&p_sel).skip(1) {
+        let plot = item.text().next().unwrap_or("");
+        info!("plot: {}", plot);
+    }
+
 }
