16044e602e9ead05b1e575d6a56b9fdd28afa2da
Journeyman <jman@storiepvtride.it>
2017-05-15 00:48:46 +0200

    fixed HTML pages parsing

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate hyper;
 
 use std::process;
 use std::io::Read;
-use std::io::Write;
+// use std::io::Write;
 use std::fs::File;
 
 use log::{LogRecord, LogLevel, LogMetadata, SetLoggerError, LogLevelFilter};
@@ -15,6 +15,8 @@ use hyper::Client;
 use hyper::header::Connection;
 use scraper::{Html, Selector};
 
+static CINEMA_URL : &'static str = "http://visionario.movie";
+const LOCAL_DEBUG : bool = false;
 struct SimpleLogger;
 
 impl log::Log for SimpleLogger {
@@ -55,39 +57,33 @@ fn main() {
              .takes_value(true))
         .get_matches();
     debug!("Got param: {}", matches.value_of("date_from").unwrap());
-
     let date = matches.value_of("date_from").unwrap();
+
+    // Get movies for the week
+    let client = Client::new();
+    let url = format!("{}/calendario-settimanale/?data={}", CINEMA_URL, date);
     let mut body = String::new();
-
-    // Get HTML file
-    // let url = format!("http://visionario.movie/calendario-settimanale/?data={}", date);
-    // let client = Client::new();
-    // let mut response = client.get(&url).
-    //     header(Connection::close()).send().unwrap_or_else(|e| {
-    //         error!("Error occurred: {}", e);
-    //         process::exit(1);
-    //     });
-    // response.read_to_string(&mut body).unwrap();
-
-    // Local debugging without hammering the (unsuspecting) website
-    let filename = format!("{}.html", date);
-    // let mut fp = File::create(&filename).unwrap();
-    // match fp.write_all(body.as_bytes()) {
-    //     Ok(_) => debug!("File written"),
-    //     Err(err) => debug!("Failed file write: {}", err)
-    // };
-    let mut f = File::open(filename).unwrap();
-    f.read_to_string(&mut body).unwrap();
+    if LOCAL_DEBUG
+    {
+        // Open the file and read content
+        let filename = format!("{}.html", date);
+        let mut f = File::open(filename).unwrap();
+        f.read_to_string(&mut body).unwrap();
+    }
+    else
+    {
+        make_request(client, url, &mut body);
+    }
 
     // "Now you have two problems" (cit. Jamie Zawinski)
-    let mut document = Html::parse_document(&body);
+    let document = Html::parse_document(&body);
+    let mut document_detail;
     let movie_row_sel = Selector::parse("div.singoloFilmInfo").unwrap();
     let title_sel = Selector::parse("div.titoloGriglia").unwrap();
     let director_sel = Selector::parse("div.registaGriglia").unwrap();
     let timetable_sel = Selector::parse("div.orari").unwrap();
     let li_sel = Selector::parse("li").unwrap();
     let href_sel = Selector::parse("a").unwrap();
-    let mut href = String::new();
     for row in document.select(&movie_row_sel) {
 
         let title_el = row.select(&title_sel).next().unwrap();
@@ -103,23 +99,46 @@ fn main() {
         }
 
         let href_el = row.select(&href_sel).next().unwrap();
-        href = format!("{}", href_el.value().attr("href").unwrap());
+        let movie_url = format!("{}", href_el.value().attr("href").unwrap());
 
         info!("title={} director={}, orari={} link= {}",
-              title, director, timetable, href);
-    }
+              title, director, timetable, movie_url);
 
-    // TODO: GET movie detail page
-    f = File::open("movie_detail.html").unwrap();
-    f.read_to_string(&mut body).unwrap();
-    document = Html::parse_document(&body);
-    // retrieve plot
-    let movie_plot_sel = Selector::parse("div.plot").unwrap();
-    let p_sel = Selector::parse("p").unwrap();
-    let movie_plot_el = document.select(&movie_plot_sel).next().unwrap();
-    for item in movie_plot_el.select(&p_sel).skip(1) {
-        let plot = item.text().next().unwrap_or("");
-        info!("plot: {}", plot);
-    }
+        // Get movie detail page
+        let url = format!("{}{}", CINEMA_URL, movie_url);
+        if LOCAL_DEBUG
+        {
+            let mut f = File::open("movie_detail.html").unwrap();
+            f.read_to_string(&mut body).unwrap();
+        }
+        else
+        {
+            // FIXME: "client" value used here after move
+            let client2 = Client::new();
+            info!("Requesting URL: {}", url);
+            body = String::from("");
+            make_request(client2, url, &mut body);
+        }
+        document_detail = Html::parse_document(&body);
 
+        // retrieve plot
+        let movie_plot_sel = Selector::parse("div.plot").unwrap();
+        let p_sel = Selector::parse("p").unwrap();
+        let movie_plot_el = document_detail.select(&movie_plot_sel).next().unwrap();
+        // TODO: movie_plot_el.select(&p_sel).take(1)
+        for item in movie_plot_el.select(&p_sel).skip(1) {
+            let plot = item.text().next().unwrap_or("");
+            info!("plot: {}", plot);
+            break;
+        }
+    }
+}
+
+fn make_request(client:Client, url:String, body:&mut String) {
+    let mut response = client.get(&url)
+        .header(Connection::close()).send().unwrap_or_else(|e| {
+            error!("Error occurred: {}", e);
+            process::exit(1);
+        });
+    response.read_to_string(body).unwrap();
 }
