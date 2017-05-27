b1c059588ce9878b63630f195ec419debf97f41e
Journeyman <jman@storiepvtride.it>
2017-05-27 18:50:08 +0200

    have rusqlite use chrono

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,19 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 extern crate rusqlite;
 extern crate chrono;
 
-static DB_PATH : &'static str = "/tmp/cinema.db";
+static DB_PATH: &'static str = "/tmp/cinema.db";
 
 pub mod db_utils {
 
     use std::path::Path;
-    use std::fs::{File,remove_file};
+    use std::fs::{File, remove_file};
     use std::io::Write;
-    use chrono::{DateTime,UTC};
+    use chrono::{DateTime, UTC};
     use rusqlite::Connection;
     use DB_PATH;
-
-    // TODO: implement Trait to mangle a DateTime from a FromSql
 
     #[derive(Debug)]
     struct Movie {
@@ -23,26 +24,27 @@ pub mod db_utils {
         plot: String,
         url: String,
         creation_date: DateTime<UTC>,
-        read_date: Option<DateTime<UTC>>
+        read_date: Option<DateTime<UTC>>,
     }
 
-    impl Default for Movie {
-        fn default() -> Movie {
-            Movie {id: None,
-                   director: String::new(), timetable: String::new(),
-                   title: String::new(), plot: String::new(),
-                   url: String::new(),
-                   creation_date: UTC::now(),
-                   read_date: None}
-        }
-    }
+    // TODO: use a Default to automatically set creation_date
+    // impl Default for Movie {
+    //     fn default() -> Movie {
+    //         Movie {id: None,
+    //                director: String::new(), timetable: String::new(),
+    //                title: String::new(), plot: String::new(),
+    //                url: String::new(),
+    //                creation_date: UTC::now(),
+    //                read_date: None}
+    //     }
+    // }
 
     pub fn init_db() {
         let db_path = Path::new(DB_PATH);
         if db_path.exists() && db_path.is_file() {
             match remove_file(db_path) {
-                Ok(_) => {},
-                Err(res) => { println!("Error while deleting: {}", res)}
+                Ok(_) => {}
+                Err(res) => error!("Error while deleting: {}", res),
             };
         }
         let conn = Connection::open(db_path).unwrap();
@@ -56,16 +58,18 @@ pub mod db_utils {
                           creation_date   TEXT NOT NULL,
                           read_date       TEXT NULL)",
                            &[]) {
-            Ok(_) => println!("Table created"),
-            Err(err) => println!("Table creation failed: {}", err),
+            Ok(_) => debug!("Table created"),
+            Err(err) => error!("Table creation failed: {}", err),
         }
     }
 
-    pub fn insert_movie(title:String, director:String, timetable:String,
-                        plot:String, url:String) {
+    pub fn insert_movie(title: String,
+                        director: String,
+                        timetable: String,
+                        plot: String,
+                        url: String) {
         let db_path = Path::new(DB_PATH);
         let conn = Connection::open(db_path).unwrap();
-        // TODO: use a Default to automatically set creation_date
         let movie = Movie {
             id: None,
             title: title,
@@ -74,15 +78,20 @@ pub mod db_utils {
             plot: plot,
             url: url,
             creation_date: UTC::now(),
-            read_date: None
+            read_date: None,
         };
+
         match conn.execute("INSERT INTO movie (title, director, timetable, \
                             plot, url, creation_date)
                       VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
-                           &[&movie.title, &movie.director, &movie.timetable,
-                             &movie.plot, &movie.url, &movie.creation_date]) {
-            Ok(inserted) => println!("{} row(s) were inserted", inserted),
-            Err(err) => println!("INSERT failed: {}", err),
+                           &[&movie.title,
+                             &movie.director,
+                             &movie.timetable,
+                             &movie.plot,
+                             &movie.url,
+                             &movie.creation_date]) {
+            Ok(inserted) => debug!("{} row(s) were inserted", inserted),
+            Err(err) => error!("INSERT failed: {}", err),
         }
     }
 
@@ -90,49 +99,68 @@ pub mod db_utils {
         let db_path = Path::new(DB_PATH);
         let conn = Connection::open(db_path).unwrap();
         let mut stmt = conn.prepare("SELECT id, title, director, timetable, \
-                                     plot, url, creation_date, read_date FROM movie").unwrap();
+                                     plot, url, creation_date, read_date FROM movie")
+            .unwrap();
         let movie_iter = stmt.query_map(&[], |row| {
-            Movie {
-                id: row.get(0),
-                title: row.get(1),
-                director: row.get(2),
-                timetable: row.get(3),
-                plot: row.get(4),
-                url: row.get(5),
-                creation_date: row.get(6),
-                read_date: row.get(7)
-            }
-        }).unwrap();
+                Movie {
+                    id: row.get(0),
+                    title: row.get(1),
+                    director: row.get(2),
+                    timetable: row.get(3),
+                    plot: row.get(4),
+                    url: row.get(5),
+                    creation_date: row.get(6),
+                    read_date: row.get(7),
+                }
+            })
+            .unwrap();
         let mut fp = File::create("feed.xml").expect("just die");
         match fp.write(b"<?xml version=\"1.0\" encoding=\"utf-8\" ?>
     <rss version=\"2.0\">
         <channel>
-            <title>storiepvtride.it feed</title>
-            <link>http://www.storiepvtride.it</link>
-            <description>Free RSS Tutorial</description>") {
-            Ok(_) => println!("Successfully written movie list header"),
-            Err(err) => {println!("Error writing movie list header: {}", err.to_string());}
+            <title>Cinema feed</title>
+            <link>http://www.storiepvtride.it/rss/feed.xml</link>
+            <description>Cinema RSS</description>") {
+            Ok(_) => debug!("Successfully written movie list header"),
+            Err(err) => {
+                error!("Error writing movie list header: {}", err.to_string());
+            }
         };
         for movie in movie_iter {
             let m = match movie {
                 Ok(x) => x,
-                Err(err) => { println!("Skipping movie: {}", err); continue; }
+                Err(err) => {
+                    error!("Skipping movie: {}", err);
+                    continue;
+                }
             };
-            match write!(fp, "<item>
+            match write!(fp,
+                         "<item>
             <title>{}</title>
                 <link>{}</link>
                 <description>{}</description>
                 <pubDate>{}</pubDate>
             </item>",
-                         m.title, m.url, m.plot, m.creation_date) {
-                Ok(_) => {println!("Successfully written movie item");}
-                Err(err) => {println!("Error writing movie item: {}", err.to_string());}
+                         m.title,
+                         m.url,
+                         m.plot,
+                         m.creation_date) {
+                Ok(_) => {
+                    debug!("Successfully written movie item");
+                }
+                Err(err) => {
+                    error!("Error writing movie item: {}", err.to_string());
+                }
             };
         }
 
         match write!(fp, "</channel></rss>") {
-            Ok(_) => {println!("Successfully written movie list tail");}
-            Err(err) => {println!("Error writing movie list tail: {}", err.to_string());}
+            Ok(_) => {
+                debug!("Successfully written movie list tail");
+            }
+            Err(err) => {
+                error!("Error writing movie list tail: {}", err.to_string());
+            }
         };
         fp.flush().unwrap();
     }
